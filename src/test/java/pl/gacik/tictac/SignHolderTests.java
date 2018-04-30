7b3ebf7cb6ae225dab9410d03f6d84f9a7417fc1
Jakub Gacik <gacik.jakub@gmail.com>
2018-04-30 16:18:53 +0200

    Added tests for TurnHolder and SignHolder class

---
+++ src/test/java/pl/gacik/tictac/SignHolderTests.java
@@ -0,0 +1,52 @@
+package pl.gacik.tictac;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.when;
+
+public class SignHolderTests {
+
+    @Mock
+    private Player player1 = Mockito.mock(Player.class);
+
+    @Mock
+    private Player player2 = Mockito.mock(Player.class);
+
+
+    @DataProvider(name = "6PairsOfPlayerName")
+    private Object[][] PairsOfPlayerName() {
+        return new Object[][] {{"Zack", "Mark"}, {"Alex", "Ash"}, {"Jane", "Marry"}, {"Kenny", "Michael"}, {"Jack", "Fuck"},
+                {"Andrew", "Joanna"}};
+    }
+
+
+    @Test(dataProvider = "6PairsOfPlayerName")
+    public void shouldReturnProperSignOwner(String name1, String name2) {
+        // given
+        when(player1.getName()).thenReturn(name1);
+        when(player2.getName()).thenReturn(name2);
+        SignHolder signHolder = new SignHolder();
+        // when
+        signHolder.attachPlayer(player1, Sign.NOUGHT);
+        signHolder.attachPlayer(player2, Sign.CROSS);
+        // then
+        Assert.assertEquals(signHolder.getSignOwner(Sign.NOUGHT).get().getName(), player1.getName());
+        Assert.assertEquals(signHolder.getSignOwner(Sign.CROSS).get().getName(), player2.getName());
+    }
+
+    @Test(dataProvider = "6PairsOfPlayerName", expectedExceptions = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenSpecifiedSignIsBooked(String name1, String name2) throws IllegalArgumentException {
+        // given
+        when(player1.getName()).thenReturn(name1);
+        when(player2.getName()).thenReturn(name2);
+        SignHolder signHolder = new SignHolder();
+        // when - then
+        signHolder.attachPlayer(player1, Sign.NOUGHT);
+        signHolder.attachPlayer(player2, Sign.NOUGHT);
+    }
+
+}
---
+++ src/test/java/pl/gacik/tictac/TurnHolderTests.java
@@ -0,0 +1,36 @@
+package pl.gacik.tictac;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.when;
+
+public class TurnHolderTests {
+
+    @Mock
+    private Player player1 = Mockito.mock(Player.class);
+
+    @Mock
+    private Player player2 = Mockito.mock(Player.class);
+
+    @Mock
+    private Player player3 = Mockito.mock(Player.class);
+
+    @Test
+    public void shouldGetNextReturnAddedPlayerAlternately() {
+        // given
+        String[] names = new String[]{"Franc", "Jane", "Alex"};
+        when(player1.getName()).thenReturn(names[0]);
+        when(player2.getName()).thenReturn(names[1]);
+        when(player3.getName()).thenReturn(names[2]);
+        TurnHolder turnHolder = new TurnHolder(player1, player2, player3);
+        // when - then
+        for (int i = 0; i < 200; i++) {
+            if (!turnHolder.getNext().getName().equals(names[i % names.length])) {
+                Assert.fail();
+            }
+        }
+    }
+}
